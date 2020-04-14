05604a15627ce77d99812e671a606794b3269927
alperyyildiz <50810809+alperyyildiz@users.noreply.github.com>
2020-04-14 22:32:09 +0300

    Update script1.py

--- script1.py
+++ script1.py
@@ -663,7 +663,7 @@ def SINGLE_RUN(trial):
     change = { 
               'CONV': {'1': {'KER': ker},
                        '2': {'KER': ker2}},
-              'OTHERS': {'1': 'lrate': learning_rate}}
+              'OTHERS': {'1': { 'lrate': learning_rate}}}
     minloss = P_OBJ.GET_MODEL(change)
 
 global P_OBJ
